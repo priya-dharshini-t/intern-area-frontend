03694785d8c9a70db357812906c10d58f272f386
priya-dharshini-t <priyadharshinipriya1975@gmail.com>
2024-08-19 19:37:27 +0530

    e

--- src/Componets/Navbar/Sidebar.jsx
+++ src/Componets/Navbar/Sidebar.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import logo from "../../Assets/logo.png"
-import './sidebar.css'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import logo from "../../Assets/logo.png";
+import './sidebar.css';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../Feature/Userslice';
 import { signOut } from 'firebase/auth';
@@ -9,159 +9,118 @@ import { auth } from '../../firebase/firebase';
 
 function Sidebar() {
     const [sidebarOpen, setSidebarOpen] = useState(false);
-const navigate=useNavigate()
-  const openSidebar = () => {
-    setSidebarOpen(true);
-  };
+    const navigate = useNavigate();
+    const user = useSelector(selectUser);
 
-  const closeSidebar = () => {
-    setSidebarOpen(false);
-  };
-
-  useEffect(() => {
-    const handleOutsideClick = (e) => {
-      if (sidebarOpen && !e.target.closest('.sidebar') && !e.target.closest('.open-btn')) {
-        closeSidebar();
-      }
+    const openSidebar = () => {
+        setSidebarOpen(true);
     };
 
-    document.addEventListener('click', handleOutsideClick);
-
-    return () => {
-      document.removeEventListener('click', handleOutsideClick);
+    const closeSidebar = () => {
+        setSidebarOpen(false);
     };
-  }, [sidebarOpen]);
-  const logoutFunction=()=>{
-    signOut(auth)
-    navigate("/")
-  
-}
-  const user=useSelector(selectUser)
-  return (
 
-    <>
-  <div className="App2 -mt-2 overflow-hidden"  >
-      <Link to="/">
-  <img src={logo} alt=""  id='nav2-img'/>    </Link>  
-        <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
-          <span  className="cursor-pointer close-btn" onClick={closeSidebar}>
-            &times;
-          </span>
-          {user?(
-  <>
-  <div className="profile">
-   <Link to={"/profile"}>
-   <img className='rounded-full justify-center' src={user.photo} alt="" srcset="" />
-   </Link> 
-    <p className=' text-center'>Profile name <span className='font-bold text-blue-500'>{user?.name}</span></p>
-  </div>
-  </>
-):
-(
+    useEffect(() => {
+        const handleOutsideClick = (e) => {
+            if (sidebarOpen && !e.target.closest('.sidebar') && !e.target.closest('.open-btn')) {
+                closeSidebar();
+            }
+        };
 
-  
-  <div className="auth">
-  
-</div>
-  ) 
-}
-          <Link to="/internship">internships </Link>
-    <Link to="/Jobs">Jobs  </Link>
-       
-       <Link to={"/"} className='small'>contact Us</Link> 
-<hr />
-{user?(
-  <>
-  <div className="addmore">
-    
-    {user?(
-  <Link to={"/userapplication"}>
-  <p>My Applications</p>
-  </Link>
-    ):(
-      <Link to={"/register"}>
-      <p>My Applications</p>
-      </Link>
-    )
+        document.addEventListener('click', handleOutsideClick);
 
-    }
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, [sidebarOpen]);
 
-  <Link>
-  
-  <p>View Resume</p>
-  </Link>
-  <Link>
-  <p>More</p>
-  </Link>
-  <button className='bt-log' id='bt' onClick={logoutFunction}>Logout <i class="bi bi-box-arrow-right"></i></button>
-  <br />
-  <br />
-<button onClick={logoutFunction}>Log Out <i class="bi bi-box-arrow-right"></i></button>
-  
-  </div>
-  </>
-):
-(
+    const logoutFunction = () => {
+        signOut(auth);
+        navigate("/");
+    };
 
-  
-  <div className="addmore">
-  <p>Register- As a Student</p>
-  <p>Register- As a Employer</p>
-  <br />
-  <br />
+    return (
+        <>
+            <div className="App2 -mt-2 overflow-hidden">
+                <Link to="/">
+                    <img src={logo} alt="logo" id='nav2-img' />
+                </Link>
 
-  </div>
-  ) 
-}
+                <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+                    <span className="cursor-pointer close-btn" onClick={closeSidebar}>
+                        &times;
+                    </span>
 
-        </div>
+                    {user ? (
+                        <div className="profile">
+                            <Link to="/profile">
+                                <img className='rounded-full justify-center' src={user.photo} alt="Profile" />
+                            </Link>
+                            <p className='text-center'>Profile name <span className='font-bold text-blue-500'>{user?.name}</span></p>
+                        </div>
+                    ) : (
+                        <div className="auth">
+                            <Link to="/login">Login</Link>
+                        </div>
+                    )}
 
+                    <nav>
+                        <Link to="/internship">Internships</Link>
+                        <Link to="/Jobs">Jobs</Link>
+                        <Link to="/contact" className='small'>Contact Us</Link>
+                        <hr />
+                        {user ? (
+                            <div className="addmore">
+                                <Link to="/userapplication">
+                                    <p>My Applications</p>
+                                </Link>
+                                <Link to="/resume">
+                                    <p>View Resume</p>
+                                </Link>
+                                <Link to="/more">
+                                    <p>More</p>
+                                </Link>
+                                <button className='bt-log' id='bt' onClick={logoutFunction}>Logout <i className="bi bi-box-arrow-right"></i></button>
+                            </div>
+                        ) : (
+                            <div className="addmore">
+                                <Link to="/register-student"><p>Register - As a Student</p></Link>
+                                <Link to="/register-employer"><p>Register - As an Employer</p></Link>
+                            </div>
+                        )}
+                    </nav>
+                </div>
 
-        <div className="main">
-          <span style={{fontSize:"22px"}} className="open-btn" onClick={openSidebar}>
-            &#9776; 
-          </span>
-        </div>
-        
-<div className="search2">
-<i class="bi bi-search"></i>
-    <input type="search"  placeholder='Search'/>
-</div>
+                <div className="main">
+                    <span style={{ fontSize: "22px" }} className="open-btn" onClick={openSidebar}>
+                        &#9776;
+                    </span>
+                </div>
 
+                <div className="search2">
+                    <i className="bi bi-search"></i>
+                    <input type="search" placeholder='Search' />
+                </div>
 
-{user?(
-  <>
+                {!user && (
+                    <>
+                        <div className="reg">
+                            <Link to="/register">
+                                <button className='btn4'>Register</button>
+                            </Link>
+                        </div>
+                        <div className="admin">
+                            <Link to="/adminLog">
+                                <button id='admin'>Admin Login</button>
+                            </Link>
+                        </div>
+                    </>
+                )}
 
-  </>
-):
-(
-
-  <>
-  <div className="reg">
-    
-  <Link to="/register" >   <button  className='btn4'>
-  Register</button></Link>
-  </div>
-  <div className="admin">
-
-<Link to={"/adminLog"}>
-<button id='admin'> Admin Login</button>
-</Link>
-</div>
-  </>
-
-
-
-  ) 
+                <p className='text-red-300'>Hire Talent</p>
+            </div>
+        </>
+    );
 }
 
-
-<p className='text-red-300'>Hire Talent</p>
-
-      </div>
-    </>
-    
-  )
-}
-
-export default Sidebar
+export default Sidebar;
